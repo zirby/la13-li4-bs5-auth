53f15cf6582ccf2bf41a74c1b01bd67512acf0de
chzirbes <chzirbes@gmail.com>
2026-06-09 11:16:21 +0200

    change theme ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,20 @@ import '../sass/app.scss'
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+
+function getTheme() {
+    return (
+        localStorage.getItem("theme") ||
+        (window.matchMedia("(prefers-color-scheme:dark").matches
+            ? "dark"
+            : " light")
+    );
+}
+document.getElementById("themeToggle").addEventListener("click", () => {
+    const currentTheme = getTheme();
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-bs-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+});
+document.documentElement.setAttribute("data-bs-theme", getTheme());
